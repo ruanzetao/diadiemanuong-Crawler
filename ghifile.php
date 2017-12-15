75b7abda1d127674da98fe2d860b9ab7cb2c0356
ruanzetao <ruanzetao@gmail.com>
2017-12-15 10:23:23 +0700

    no message

--- ghifile.php
+++ ghifile.php
@@ -7,6 +7,49 @@ require "Classes/PHPExcel.php";
 
 set_time_limit(0);
 
+function convert_vi_to_en($str) {
+  // $str = preg_replace(“/(à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ)/”, ‘a’, $str);
+  // $str = preg_replace(“/(è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ)/”, ‘e’, $str);
+  // $str = preg_replace(“/(ì|í|ị|ỉ|ĩ)/”, ‘i’, $str);
+  // $str = preg_replace(“/(ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ)/”, ‘o’, $str);
+  // $str = preg_replace(“/(ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ)/”, ‘u’, $str);
+  // $str = preg_replace(“/(ỳ|ý|ỵ|ỷ|ỹ)/”, ‘y’, $str);
+  // $str = preg_replace(“/(đ)/”, ‘d’, $str);
+  // $str = preg_replace(“/(À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ)/”, ‘A’, $str);
+  // $str = preg_replace(“/(È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ)/”, ‘E’, $str);
+  // $str = preg_replace(“/(Ì|Í|Ị|Ỉ|Ĩ)/”, ‘I’, $str);
+  // $str = preg_replace(“/(Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ)/”, ‘O’, $str);
+  // $str = preg_replace(“/(Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ)/”, ‘U’, $str);
+  // $str = preg_replace(“/(Ỳ|Ý|Ỵ|Ỷ|Ỹ)/”, ‘Y’, $str);
+  // $str = preg_replace(“/(Đ)/”, ‘D’, $str);
+  // //$str = str_replace(” “, “-”, str_replace(“&*#39;”,”",$str));
+  // return $str;
+
+	$unicode = array(
+
+		'a'=>'á|à|ả|ã|ạ|ă|ắ|ặ|ằ|ẳ|ẵ|â|ấ|ầ|ẩ|ẫ|ậ',
+		'd'=>'đ',
+		'e'=>'é|è|ẻ|ẽ|ẹ|ê|ế|ề|ể|ễ|ệ',
+		'i'=>'í|ì|ỉ|ĩ|ị',
+		'o'=>'ó|ò|ỏ|õ|ọ|ô|ố|ồ|ổ|ỗ|ộ|ơ|ớ|ờ|ở|ỡ|ợ',
+		'u'=>'ú|ù|ủ|ũ|ụ|ư|ứ|ừ|ử|ữ|ự',
+		'y'=>'ý|ỳ|ỷ|ỹ|ỵ',
+		'A'=>'Á|À|Ả|Ã|Ạ|Ă|Ắ|Ặ|Ằ|Ẳ|Ẵ|Â|Ấ|Ầ|Ẩ|Ẫ|Ậ',
+		'D'=>'Đ',
+		'E'=>'É|È|Ẻ|Ẽ|Ẹ|Ê|Ế|Ề|Ể|Ễ|Ệ',
+		'I'=>'Í|Ì|Ỉ|Ĩ|Ị',
+		'O'=>'Ó|Ò|Ỏ|Õ|Ọ|Ô|Ố|Ồ|Ổ|Ỗ|Ộ|Ơ|Ớ|Ờ|Ở|Ỡ|Ợ',
+		'U'=>'Ú|Ù|Ủ|Ũ|Ụ|Ư|Ứ|Ừ|Ử|Ữ|Ự',
+		'Y'=>'Ý|Ỳ|Ỷ|Ỹ|Ỵ',
+
+	);
+
+	foreach($unicode as $nonUnicode=>$uni){
+		$str = preg_replace("/($uni)/i", $nonUnicode, $str);
+	}
+	return $str;
+}
+
 for($t=0;$t<=10;$t++){
 	GetFood($t);
 }
@@ -25,10 +68,11 @@ function GetFood($trang){
 		$url=$cuahang->href;
 		echo $url;
 		echo "<br>";
-		$tieude=$cuahang->find("h3.ddd",0)->innertext();
+		$tieude=utf8_decode(convert_vi_to_en($cuahang->find("h3.ddd",0)->innertext()));
 		echo $tieude;
 		echo "<br>";
-		$tieude = utf8_decode($tieude);
+		// convert_vi_to_en($tieude);
+		// $tieude = utf8_decode($tieude);
 		$danhsao=$cuahang->find("div.star",0)->getAttribute("data-star-value");
 		// echo $danhsao;
 		// echo "<br>";
@@ -38,8 +82,9 @@ function GetFood($trang){
 		$gia=$cuahang->find("span.price",0)->innertext();
 		// echo $gia;
 		// echo "<br>";
-		$diachi=$cuahang->find("span.address",0)->innertext();
-		$diachi = utf8_decode($diachi);
+		$diachi=utf8_decode(convert_vi_to_en($cuahang->find("span.address",0)->innertext()));
+		// convert_vi_to_en($diachi);
+		// $diachi = utf8_decode($diachi);
 		// echo $diachi;
 		// echo "<br>";
 		// $daxem=$cuahang->find("div.summary span",0)->innertext();
@@ -54,13 +99,13 @@ function GetFood($trang){
 		$htmldetail=file_get_html("http://diadiemanuong.com".$url);
 		$datadetail=$htmldetail->find("ul.totalCharts");
 		foreach($datadetail as $detail){
-			$dichvu=$detail->find("li ul li div",0)->getAttribute("data-star-value");
+			$dichvu=$detail->find("li ul li div.star",0)->getAttribute("data-star-value");
 			// echo $dichvu;
 			// echo "<br>";
-			$giaca=$detail->find("li ul li div",1)->getAttribute("data-star-value");
+			$giaca=$detail->find("li ul li div.star",1)->getAttribute("data-star-value");
 			// echo $giaca;
 			// echo "<br>";
-			$khonggian=$detail->find("li ul li div",2)->getAttribute("data-star-value");
+			$khonggian=$detail->find("li ul li div.star",2)->getAttribute("data-star-value");
 			// echo $khonggian;
 			// echo "<br>";
 
@@ -101,27 +146,27 @@ function GetFood($trang){
 	}
 
 	//Khởi tạo đối tượng
-$excel = new PHPExcel();
+	$excel = new PHPExcel();
 //Chọn trang cần ghi (là số từ 0->n)
-$excel->setActiveSheetIndex(0);
+	$excel->setActiveSheetIndex(0);
 //Tạo tiêu đề cho trang. (có thể không cần)
-$excel->getActiveSheet()->setTitle('Data Crawl');
+	$excel->getActiveSheet()->setTitle('Data Crawl');
 //Xét chiều rộng cho từng, nếu muốn set height thì dùng setRowHeight()
-$excel->getActiveSheet()->getColumnDimension('A')->setWidth(15);
-$excel->getActiveSheet()->getColumnDimension('B')->setWidth(15);
-$excel->getActiveSheet()->getColumnDimension('C')->setWidth(15);
-$excel->getActiveSheet()->getColumnDimension('D')->setWidth(15);
-$excel->getActiveSheet()->getColumnDimension('E')->setWidth(15);
-$excel->getActiveSheet()->getColumnDimension('F')->setWidth(15);
-$excel->getActiveSheet()->getColumnDimension('G')->setWidth(15);
-$excel->getActiveSheet()->getColumnDimension('H')->setWidth(15);
-$excel->getActiveSheet()->getColumnDimension('I')->setWidth(15);
-$excel->getActiveSheet()->getColumnDimension('J')->setWidth(15);
-$excel->getActiveSheet()->getColumnDimension('K')->setWidth(15);
-$excel->getActiveSheet()->getColumnDimension('L')->setWidth(15);
-$excel->getActiveSheet()->getColumnDimension('M')->setWidth(15);
+	$excel->getActiveSheet()->getColumnDimension('A')->setWidth(15);
+	$excel->getActiveSheet()->getColumnDimension('B')->setWidth(15);
+	$excel->getActiveSheet()->getColumnDimension('C')->setWidth(15);
+	$excel->getActiveSheet()->getColumnDimension('D')->setWidth(15);
+	$excel->getActiveSheet()->getColumnDimension('E')->setWidth(15);
+	$excel->getActiveSheet()->getColumnDimension('F')->setWidth(15);
+	$excel->getActiveSheet()->getColumnDimension('G')->setWidth(15);
+	$excel->getActiveSheet()->getColumnDimension('H')->setWidth(15);
+	$excel->getActiveSheet()->getColumnDimension('I')->setWidth(15);
+	$excel->getActiveSheet()->getColumnDimension('J')->setWidth(15);
+	$excel->getActiveSheet()->getColumnDimension('K')->setWidth(15);
+	$excel->getActiveSheet()->getColumnDimension('L')->setWidth(15);
+	$excel->getActiveSheet()->getColumnDimension('M')->setWidth(15);
 //Xét in đậm cho khoảng cột
-$excel->getActiveSheet()->getStyle('A1:M1')->getFont()->setBold(true);
+	$excel->getActiveSheet()->getStyle('A1:M1')->getFont()->setBold(true);
 //Tạo tiêu đề cho từng cột
 //Vị trí có dạng như sau:
 /**
@@ -130,21 +175,21 @@ $excel->getActiveSheet()->getStyle('A1:M1')->getFont()->setBold(true);
  * |..|..|..|..|..|
  * |An|Bn|Cn|..|nn|
  */
- $excel->getActiveSheet()->setCellValue('A1', 'TieuDe');
- $excel->getActiveSheet()->setCellValue('B1', 'DanhSao');
- $excel->getActiveSheet()->setCellValue('C1', 'Gia');
- $excel->getActiveSheet()->setCellValue('D1', 'DiaChi');
- $excel->getActiveSheet()->setCellValue('E1', 'DichVu');
- $excel->getActiveSheet()->setCellValue('F1', 'GiaCa');
- $excel->getActiveSheet()->setCellValue('G1', 'KhongGian');
- $excel->getActiveSheet()->setCellValue('H1', 'LuotXem');
- $excel->getActiveSheet()->setCellValue('I1', 'ChoNhanXet');
- $excel->getActiveSheet()->setCellValue('J1', 'TuyetChieu');
- $excel->getActiveSheet()->setCellValue('K1', 'BaoODay');
- $excel->getActiveSheet()->setCellValue('L1', 'BaoXau');
- $excel->getActiveSheet()->setCellValue('M1', 'LuotThich');
+$excel->getActiveSheet()->setCellValue('A1', 'TieuDe');
+$excel->getActiveSheet()->setCellValue('B1', 'DanhSao');
+$excel->getActiveSheet()->setCellValue('C1', 'Gia');
+$excel->getActiveSheet()->setCellValue('D1', 'DiaChi');
+$excel->getActiveSheet()->setCellValue('E1', 'DichVu');
+$excel->getActiveSheet()->setCellValue('F1', 'GiaCa');
+$excel->getActiveSheet()->setCellValue('G1', 'KhongGian');
+$excel->getActiveSheet()->setCellValue('H1', 'LuotXem');
+$excel->getActiveSheet()->setCellValue('I1', 'ChoNhanXet');
+$excel->getActiveSheet()->setCellValue('J1', 'TuyetChieu');
+$excel->getActiveSheet()->setCellValue('K1', 'BaoODay');
+$excel->getActiveSheet()->setCellValue('L1', 'BaoXau');
+$excel->getActiveSheet()->setCellValue('M1', 'LuotThich');
 
- $numRow = 2;
+$numRow = 2;
 foreach($array_data as $row){
 	//$tieude,$danhsao,$gia,$diachi,$dichvu,$giaca,$khonggian,$luotxem,$chonhanxet,$tuyetchieu,$baooday,$baoxau,$luotthich
 	
@@ -205,8 +250,8 @@ PHPExcel_IOFactory::createWriter($excel, 'Excel2007')->save('data.xlsx');
 	// 		->setCellValue('K1', "BaoODay")
 	// 		->setCellValue('L1', "BaoXau")
 	// 		->setCellValue('M1', "LuotThich");
-			
-		
+
+
 	// $i=2;
 	// foreach ($array_data as $value) {
 	// 	$objPHPExcel->setActiveSheetIndex(0)
